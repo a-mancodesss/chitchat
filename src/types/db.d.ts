681f6a35a4de71312bf0c2ca664667b75c5027f4
a-mancodesss <079bct011.aman@pcampus.edu.np>
2024-11-03 23:29:55 +0545

    sideBarChatList

--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -1,6 +1,25 @@
-export interface User{
-    name:string;
-    email:string;
-    image:string;
-    id:string;
-}
+interface User {
+    name: string
+    email: string
+    image: string
+    id: string
+  }
+  
+  interface Chat {
+    id: string
+    messages: Message[]
+  }
+  
+  interface Message {
+    id: string
+    senderId: string
+    receiverId: string
+    text: string
+    timestamp: number
+  }
+  
+  interface FriendRequest {
+    id: string
+    senderId: string
+    receiverId: string
+  }
